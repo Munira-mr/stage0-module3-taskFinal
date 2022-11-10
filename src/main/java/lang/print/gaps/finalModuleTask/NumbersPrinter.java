0d45ab898fd20dd82625fcaee2fcb293975c4bd4
Munira <79731097+Munira-mr@users.noreply.github.com>
2022-11-10 11:05:56 -0500

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,10 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        if(number<0){
-            System.out.println(false);
-        }else {
+        if(number>=0){
             System.out.println(true);
+        }else {
+            System.out.println(false);
         }
 
     }
